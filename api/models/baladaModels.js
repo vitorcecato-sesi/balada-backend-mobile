ba493df0e1d2268bd5196b9b3e59bc56d7fa142d
Milena <mile.26souza08@gmail.com>
2025-09-10 13:39:21 -0300

    buscar por cidade

--- api/models/baladaModels.js
+++ api/models/baladaModels.js
@@ -29,11 +29,29 @@ callback(err, row);
 }); 
 } 
 
+// Função para buscar uma balada por Data 
+function getBaladaByData(data, callback) { 
+const db = openDbConnection(); 
+db.get("SELECT * FROM baladas WHERE data = ?", [data], (err, row) => { 
+db.close(); 
+callback(err, row); 
+}); 
+} 
+
+// Função para buscar uma balada por Cidade 
+function getBaladaByCidade(cidade, callback) { 
+const db = openDbConnection(); 
+db.get("SELECT * FROM baladas WHERE cidade = ?", [cidade], (err, row) => { 
+db.close(); 
+callback(err, row); 
+}); 
+} 
+
 // Função para criar uma nova balada
 function createBalada(balada, callback) { 
 const { nome , endereco , data , tipo } = balada; 
 const db = openDbConnection(); 
-db.run("INSERT INTO baladas (nome, endereco, data, tipo  ) VALUES (?, ?, ?, ?)", [ nome , endereco , data ,  tipo], function (err) { 
+db.run("INSERT INTO baladas (nome, cidade, data, tipo  ) VALUES (?, ?, ?, ?)", [ nome , cidade , data ,  tipo], function (err) { 
 db.close(); 
 callback(err, { id: this.lastID }); 
 }); 
@@ -43,7 +61,7 @@ callback(err, { id: this.lastID });
 function updateBalada(id, cliente, callback) { 
 const { nome , endereco , data , tipo  } = cliente; 
 const db = openDbConnection(); 
-db.run("UPDATE baladas SET nome = ?, endereco = ?, data = ?, tipo = ?  WHERE id = ?", [nome , endereco , data,  tipo , id], function (err) { 
+db.run("UPDATE baladas SET nome = ?, cidade = ?, data = ?, tipo = ?  WHERE id = ?", [nome , endereco , data,  tipo , id], function (err) { 
 db.close(); 
 callback(err, { changes: this.changes }); 
 }); 
