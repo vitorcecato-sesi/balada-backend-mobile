fab85993320c9c3e0ab0ad46e7dde6bf15a54364
Vitor Cecato <vitor.cecatosesi@gmail.com>
2025-09-17 13:44:03 -0300

    Base

--- App.js
+++ App.js
@@ -1,14 +1,48 @@
-// app.js 
-const express = require('express'); 
-const app = express(); 
-const port = 3000; 
-// Middleware para analisar o corpo das requisições em JSON 
-app.use(express.json()); 
-// Importando as rotas do cliente 
-const clienteRoutes = require('./routes/baladas'); 
-// Usando as rotas do cliente com o prefixo '/clientes' 
-app.use('/baladas', baladasRoutes); 
-// Iniciando o servidor na porta especificada 
-app.listen(port, () => { 
-console.log(`Servidor rodando em http://localhost:${port}`); 
-});
+/* Imports para a Navegação */
+import { NavigationContainer } from '@react-navigation/native' /* é um "container" que envolve toda a navegação do app */
+import { createNativeStackNavigator } from '@react-navigation/native-stack' /* ele cria uma "pilha" de telas (Stack) para navegar entre elas. */
+
+/* Imports das telas do App */
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+    return (
+        <NavigationContainer>
+
+            <Stack.Navigator initialRouteName="Menu">
+
+                <Stack.Screen
+                    name="Menu"                 
+                    component={Menu}        
+                    options={{ title: 'Menu' }} 
+                />
+
+                <Stack.Screen
+                    name="Buscar"                   
+                    component={Buscar}    
+                    options={{ title: 'Buscar' }}
+                />
+
+                <Stack.Screen
+                    name="Inserir"                
+                    component={Inserir}     
+                    options={{ title: 'Inserir' }}
+                />
+
+                <Stack.Screen 
+                    name='Atualizar'
+                    component={Atualizar}
+                    options={{ title: 'Atualizar' }}
+                />
+
+                <Stack.Screen 
+                    name='Deletar'
+                    component={Deletar}
+                    options={{ title: 'Deletar' }}
+                />
+
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
---
+++ screens/Inserir.js
@@ -0,0 +1,6 @@
+export default function Inserir({ navigation }) {
+    return (
+        <>
+        </>
+    )
+}
